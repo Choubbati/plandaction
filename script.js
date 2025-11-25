eef624966598b72ebe34c7b9f4bfa39d844d6a78
choubbati <choubbati@gmail.com>
2025-11-25 15:05:20 +0100

    exercice 1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// ============== exercice 1======================
+// let storage =localStorage.setItem("name","ali")
+
+// let getData = localStorage.getItem('name')
+
+// console.log(getData);
+
+
+// ============== Exercice 2 ======================
+
