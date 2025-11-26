f65199cdd543cd8551cad60ea8ea48d3510d2595
choubbati <choubbati@gmail.com>
2025-11-26 21:00:11 +0100

    Partie 6 : Mise à jour dynamique

--- script.js
+++ script.js
@@ -1,14 +1,238 @@
 
 
+// // ============== exercice 1======================
+// // let storage =localStorage.setItem("name","ali")
+
+// // let getData = localStorage.getItem('name')
+
+// // console.log(getData);
+
+
+// // ============== Exercice 2 ======================
+
+
+// const person = { name: "Alex" };
+// localStorage.setItem("user", person);
+// console.log(localStorage.getItem("user")); // "[object Object]"; not useful!
+
+// // ====================
+
+// localStorage.setItem("user", JSON.stringify(person));
+// console.log(JSON.parse(localStorage.getItem("user"))); // { name: "Alex" }
+
+
+
+// // remove key user 
+// localStorage.removeItem("user")
+
+
+// // vide localstorage
+
+// localStorage.setItem('name',"chouaib")
+// localStorage.setItem('prenom',"loubbati")
+// localStorage.setItem('age',30)
+
+// console.log(localStorage.getItem('name'));
+// console.log(localStorage.getItem('prenom'));
+// console.log(localStorage.getItem('age'));
+
+
+
+// // afficher les keys 
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   const keyName = localStorage.key(i);
+//   console.log(keyName);
+// }
+
+
+// // delete all data in localstorage
+// localStorage.clear()
+
+
+
+// // ============== Exercice 3 ======================
+
+
+
+// const joueur = { id: 1, name: "Salah" };
+// localStorage.setItem("joueur1", JSON.stringify(joueur));
+
+// const joueurRecup = JSON.parse(localStorage.getItem("joueur1"));
+// console.log(joueurRecup.id, joueurRecup.name);
+
+// const joueurs = [
+//   { id: 1, name: "Salah" },
+//   { id: 2, name: "Amine" },
+//   { id: 3, name: "Youssef" }
+// ];
+
+
+// localStorage.setItem("joueurs",JSON.stringify(joueurs))
+// let tab = JSON.parse(localStorage.getItem("joueurs"))
+
+// console.log(tab)
+
+
+
+// if(tab){
+//   const index = tab.findIndex(a => a.id ===2)
+
+//   if (index !== -1) {
+//     tab[index].name = "amine est modifiér"
+//   }else{
+//     console.warn("pas de joueur")
+//   }
+
+//   localStorage.setItem("joueurs",JSON.stringify(tab))
+
+// }
+
+// const newTab = JSON.parse(localStorage.getItem("joueurs"))
+
+// console(newTab)
+
+
+
+// // ============== Partie 4 :  Mini gestion CRUD (console)======================
+
+
+// const joueurss = [
+//   { id: 1, name: "Salah" },
+//   { id: 2, name: "Amine" },
+//   { id: 3, name: "Youssef" }
+// ];
+
+
+// // ajouter un joueur
+
+// localStorage.setItem("jou",JSON.stringify(joueurss))
+
+// let a = {id: 4, name: "chouaib"}
+
+// let tabe = JSON.parse(localStorage.getItem("jou")) || []
+// tabe.push(a)
+// localStorage.setItem('jou',JSON.stringify(tabe))
+// const jr = JSON.parse(localStorage.getItem("jou"))
+
+// console.log(jr)
+
+// // suprimer joueur
+
+// const supr = tabe.findIndex(e => e.id === 1);
+
+// if (supr !== -1) {
+//   tabe.splice(supr, 1); 
+// }
+
+// localStorage.setItem("jou", JSON.stringify(tabe));
+
+// console.log("Après suppression :", JSON.parse(localStorage.getItem("jou")));
+
+// // modifier
+
+// const list = JSON.parse(localStorage.getItem('jou')) || []
+
+// const indx = list.findIndex(e=> e.id === 2)
+
+// if (indx !== -1) {
+//   list[indx].name = "modif"
+//   localStorage.setItem("jou",JSON.stringify(list))
+//   // afficher tout les joueurs
+//   console.log("modifier seccuss",list)
+// }else{
+//   console.warn('pas de joueurs')
+// }
+
+
+
+// Partie 5 :  DOM : lecture/écriture
+
+
+// function getPersonne() {
+//   const storage = localStorage.getItem("joueur")
+
+//   if (storage) {
+//     try{
+//       return JSON.parse(storage)
+
+//     }catch (e){
+//       console.error("error parse JSON : ",e)
+//       return []
+//     }
+//   }
+//   return []
+// }
+
+
+// function SavePersonne(list) {
+//     localStorage.setItem('joueur', JSON.stringify(list))
+// }
+
+
+// function renderPerssone() {
+//     const list = getPersonne()
+//     const container = document.getElementById("personList");
+//     container.innerHTML = ""
+
+//     if (list.length  === 0) {
+//       container.innerHTML = "pas de personne"
+//       return
+//     }
+
+//     list.forEach((p , index) => {
+//         const el = document.createElement("div")
+//         el.className = 'person'
+
+//     const text = document.createElement('span');
+//     text.textContent = `${index + 1}. ${p.name} — ${p.age} ans`;
+
+//     el.appendChild(text)
+// container.appendChild(el)
+//     });
+
+
+// }
+
+
+
+// document.getElementById("personForm").addEventListener("submit", function(e) {
+//   e.preventDefault()
+
+//   const name = document.getElementById("nameInput").value.trim()
+//   const age = document.getElementById('ageInput').value.trim()
+
+
+//   if(!name || age === ''){
+//     alert('remplir name et age')
+//     return
+//   }
+
+//   const pep = getPersonne()
+//   pep.push({name: name , age: parseInt(age ,10)})
+
+//   SavePersonne(pep)
+//   this.reset()
+
+//   renderPerssone()
+
+// })
+
+// window.addEventListener("DOMContentLoaded", renderPerssone)
+
+
+
+
+
 function getPersonne() {
   const storage = localStorage.getItem("joueur")
 
   if (storage) {
-    try{
+    try {
       return JSON.parse(storage)
 
-    }catch (e){
-      console.error("error parse JSON : ",e)
+    } catch (e) {
+      console.error("error parse JSON : ", e)
       return []
     }
   }
@@ -17,50 +241,72 @@ function getPersonne() {
 
 
 function SavePersonne(list) {
-    localStorage.setItem('joueur', JSON.stringify(list))
+  localStorage.setItem('joueur', JSON.stringify(list))
 }
 
 
 function renderPerssone() {
-    const list = getPersonne()
-    const container = document.getElementById("personList");
-    container.innerHTML = ""
+  const list = getPersonne()
+  const container = document.getElementById("personList");
+  container.innerHTML = ""
 
-    if (list.length  === 0) {
-      container.innerHTML = "pas de personne"
-      return
-    }
+  if (list.length === 0) {
+    container.innerHTML = "pas de personne"
+    return
+  }
 
-    list.forEach((p , index) => {
-        const el = document.createElement("div")
-        el.className = 'person'
-        
+  list.forEach((p, index) => {
+    const el = document.createElement("div")
+    el.className = 'person'
+
     const text = document.createElement('span');
     text.textContent = `${index + 1}. ${p.name} — ${p.age} ans`;
 
-    el.appendChild(text)
-container.appendChild(el)
-    });
+    const btns= document.createElement("button")
+    btns.className = "btnSuprimer"
 
-    
+    btns.textContent = "suprimer"
+
+    btns.addEventListener('click' , ()=>{
+    suprimerPersonne(index)
+
+    })
+
+
+    el.appendChild(text)
+    el.appendChild(btns)
+
+    container.appendChild(el)
+  });
+
+
+}
+
+function  suprimerPersonne(index) {
+    const list = getPersonne()
+
+    list.splice(index ,1)
+    SavePersonne(list)
+    renderPerssone()
 }
 
 
 
-document.getElementById("personForm").addEventListener("submit", function(e) {
+
+document.getElementById("personForm").addEventListener("submit", function (e) {
   e.preventDefault()
 
   const name = document.getElementById("nameInput").value.trim()
   const age = document.getElementById('ageInput').value.trim()
 
 
-  if(!name || age === ''){
+  if (!name || age === '') {
     alert('remplir name et age')
     return
   }
 
   const pep = getPersonne()
-  pep.push({name: name , age: parseInt(age ,10)})
+  pep.push({ name: name, age: parseInt(age, 10) })
 
   SavePersonne(pep)
   this.reset()
